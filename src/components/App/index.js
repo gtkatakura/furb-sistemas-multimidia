74a19e2057617288ced3e8a2ef89ab49beae6e42
gtkatakura <gt.katakura@gmail.com>
2017-11-30 00:25:40 -0200

    feat(cache): login criado

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -8,18 +8,39 @@ import GameSet from '../GameSet';
 
 import './index.less';
 
-const Root = () => (
-  <Router>
-    <div id="outer-container">
-      <Menu />
-      <main id="page-wrap">
-        <Switch>
-          <Route exact path="/" component={Rank} />
-          <Route path="/exercises" component={GameSet} />
-        </Switch>
-      </main>
-    </div>
-  </Router>
-);
+export default class App extends React.Component {
+  constructor() {
+    super();
 
-export default Root;
+    this.state = {
+      userName: null,
+    };
+  }
+
+  onClick() {
+    this.setState({
+      userName: this.nameField.value,
+    });
+  }
+
+  render() {
+    return (
+      <Router>
+        {this.state.userName ? (<div id="outer-container">
+          <Menu />
+          <main id="page-wrap">
+            <Switch>
+              <Route exact path="/" component={Rank} />
+              <Route path="/exercises" component={GameSet} />
+            </Switch>
+          </main>
+        </div>) : (<main>
+          <div className="inner">
+            <input ref={el => { this.nameField = el; }} className="form-control" type="text" placeholder="Apelido" autoFocus />
+            <button type="button" className="btn btn-success margin-top" onClick={this.onClick.bind(this)}>Jogar</button>
+          </div>
+        </main>)}
+      </Router>
+    )
+  }
+}
